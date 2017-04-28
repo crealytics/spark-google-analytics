00b79968a6414a23234e79d74516c86ace130497
Jay Panchal <panchaljay.2010@gmail.com>
2017-04-28 18:15:06 +0530

    Adding alternate option for client authentication User can pass 'clientId', 'clientSecret' and 'refreshToken' as an authentication with Set Of Authentication Method 1: 'serviceAccountId', 'keyFileLocation'
    OR 
    Set Of Authentication Method 2: 'clientId', 'clientSecret' and
    'refreshToken'

--- src/main/scala/com/crealytics/google/analytics/DefaultSource.scala
+++ src/main/scala/com/crealytics/google/analytics/DefaultSource.scala
@@ -1,6 +1,5 @@
 package com.crealytics.google.analytics
 
-
 import java.io.File
 import java.util
 
@@ -10,6 +9,7 @@ import com.google.api.client.json.gson.GsonFactory
 import com.google.api.services.analytics.{Analytics, AnalyticsScopes}
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.sources._
+import com.google.api.client.auth.oauth2.Credential
 
 class DefaultSource
   extends RelationProvider {
@@ -20,20 +20,82 @@ class DefaultSource
   override def createRelation(
     sqlContext: SQLContext,
     parameters: Map[String, String]): AnalyticsRelation = {
-    val keyFileLocation = parameters("keyFileLocation")
-    val serviceAccountId = parameters("serviceAccountId")
-    val credential = new GoogleCredential.Builder().setTransport(httpTransport)
-      .setJsonFactory(jsonFactory)
-      .setServiceAccountId(serviceAccountId)
-      .setServiceAccountPrivateKeyFromP12File(new File(keyFileLocation))
-      .setServiceAccountScopes(util.Arrays.asList(AnalyticsScopes.ANALYTICS_READONLY))
-      .build()
+   
+    /*** Local initialization ***/
+    
+    /*** Authentication Method Field Set - 1 */
+    //Client ID
+    var clientId: String = null
+    
+    //Client Secret
+    var clientSecret: String = null
+    
+    //Refresh Token
+    var refreshToken: String = null
+
+    /*** Authentication Method Field Set - 2 */
+    //Service Account ID
+    var serviceAccountId: String = null
+    
+    //Key File Location (.p12)
+    var keyFileLocation: String = null
+
+    //Credential Object 
+    var credential: Credential = null
+
+    println("Building Credentials")
+
+    if (parameters.contains("clientId") && parameters.contains("clientSecret") && parameters.contains("refreshToken")) {
+
+      //Build Credential Object
+      println("Credentials will be build based on Clinet ID, Client Secret and Refresh Token")
+
+      clientId = parameters("clientId")
+      clientSecret = parameters("clientSecret")
+      refreshToken = parameters("refreshToken")
+
+      credential = new GoogleCredential.Builder().setTransport(httpTransport)
+        .setJsonFactory(jsonFactory)
+        .setClientSecrets(clientId, clientSecret)
+        .build()
+
+      credential.setRefreshToken(refreshToken)
+
+    } else if (parameters.contains("serviceAccountId") && parameters.contains("keyFileLocation")) {
+
+      //Build Credential Object
+      println("Credentials will be build based on Service Account ID and Key File Location")
+
+      keyFileLocation = parameters("keyFileLocation")
+      serviceAccountId = parameters("serviceAccountId")
+      
+      credential = new GoogleCredential.Builder().setTransport(httpTransport)
+        .setJsonFactory(jsonFactory)
+        .setServiceAccountId(serviceAccountId)
+        .setServiceAccountPrivateKeyFromP12File(new File(keyFileLocation))
+        .setServiceAccountScopes(util.Arrays.asList(AnalyticsScopes.ANALYTICS_READONLY))
+        .build()
+    } else {
+
+      println("Incomplete credential information. Please provide valid credentials.")
+      throw new Exception("Incomplete credential information. Please provide valid credentials. See README file instructions for more info.")
+
+    }
+
+    println("Building Analytics Object")
+
+    //Build Analytics Object
     val analytics = new Analytics.Builder(httpTransport, jsonFactory, credential)
       .setApplicationName("spark-google-analytics")
       .build()
+
+    //Calculated Metrics
     val calculatedMetrics = parameters.getOrElse("calculatedMetrics", "").split(",").map(_.trim)
+    
+    //Query Individual Days (Boolean)
     val queryIndividualDays: Boolean = parameters.getOrElse("queryIndividualDays", "false") == "true"
 
+    //Call to AnalyticsRelation
     AnalyticsRelation(
       analytics,
       parameters("ids"),
