ddbe5e550bea937dcf4e48a79c590d9c483b46c7
Jay Panchal <panchaljay.2010@gmail.com>
2017-05-01 15:14:57 +0530

    Code changes recommended by @nightscape and Updated README file.
    User can pass 'clientId', 'clientSecret' and 'refreshToken' as an
    authentication with
    Set Of Authentication Method 1: 'serviceAccountId', 'keyFileLocation'
    
    OR 
    Set Of Authentication Method 2: 'clientId', 'clientSecret' and
    'refreshToken'

--- src/main/scala/com/crealytics/google/analytics/DefaultSource.scala
+++ src/main/scala/com/crealytics/google/analytics/DefaultSource.scala
@@ -20,82 +20,40 @@ class DefaultSource
   override def createRelation(
     sqlContext: SQLContext,
     parameters: Map[String, String]): AnalyticsRelation = {
-   
-    /*** Local initialization ***/
-    
-    /*** Authentication Method Field Set - 1 */
-    //Client ID
-    var clientId: String = null
-    
-    //Client Secret
-    var clientSecret: String = null
-    
-    //Refresh Token
-    var refreshToken: String = null
-
-    /*** Authentication Method Field Set - 2 */
-    //Service Account ID
-    var serviceAccountId: String = null
-    
-    //Key File Location (.p12)
-    var keyFileLocation: String = null
-
-    //Credential Object 
-    var credential: Credential = null
-
-    println("Building Credentials")
-
-    if (parameters.contains("clientId") && parameters.contains("clientSecret") && parameters.contains("refreshToken")) {
-
-      //Build Credential Object
-      println("Credentials will be build based on Clinet ID, Client Secret and Refresh Token")
-
-      clientId = parameters("clientId")
-      clientSecret = parameters("clientSecret")
-      refreshToken = parameters("refreshToken")
-
-      credential = new GoogleCredential.Builder().setTransport(httpTransport)
+    val credentialsFromSecretAndToken = for {
+      clientId <- parameters.get("clientId")
+      clientSecret <- parameters.get("clientSecret")
+      refreshToken <- parameters.get("refreshToken")
+    } yield {
+      val credentials = new GoogleCredential.Builder().setTransport(httpTransport)
         .setJsonFactory(jsonFactory)
         .setClientSecrets(clientId, clientSecret)
         .build()
+      credentials.setRefreshToken(refreshToken)
+      credentials
+    }
 
-      credential.setRefreshToken(refreshToken)
-
-    } else if (parameters.contains("serviceAccountId") && parameters.contains("keyFileLocation")) {
-
-      //Build Credential Object
-      println("Credentials will be build based on Service Account ID and Key File Location")
-
-      keyFileLocation = parameters("keyFileLocation")
-      serviceAccountId = parameters("serviceAccountId")
-      
-      credential = new GoogleCredential.Builder().setTransport(httpTransport)
+    val credentialsFromKeyFile = for {
+      keyFileLocation <- parameters.get("keyFileLocation")
+      serviceAccountId <- parameters.get("serviceAccountId")
+    } yield {
+      new GoogleCredential.Builder().setTransport(httpTransport)
         .setJsonFactory(jsonFactory)
         .setServiceAccountId(serviceAccountId)
         .setServiceAccountPrivateKeyFromP12File(new File(keyFileLocation))
         .setServiceAccountScopes(util.Arrays.asList(AnalyticsScopes.ANALYTICS_READONLY))
         .build()
-    } else {
-
-      println("Incomplete credential information. Please provide valid credentials.")
-      throw new Exception("Incomplete credential information. Please provide valid credentials. See README file instructions for more info.")
-
     }
 
-    println("Building Analytics Object")
-
-    //Build Analytics Object
-    val analytics = new Analytics.Builder(httpTransport, jsonFactory, credential)
+    val credentials = credentialsFromSecretAndToken
+      .orElse(credentialsFromKeyFile)
+      .getOrElse(
+        throw new Exception("Please provide valid credentials information. See README file for more info."))
+    val analytics = new Analytics.Builder(httpTransport, jsonFactory, credentials)
       .setApplicationName("spark-google-analytics")
       .build()
-
-    //Calculated Metrics
     val calculatedMetrics = parameters.getOrElse("calculatedMetrics", "").split(",").map(_.trim)
-    
-    //Query Individual Days (Boolean)
     val queryIndividualDays: Boolean = parameters.getOrElse("queryIndividualDays", "false") == "true"
-
-    //Call to AnalyticsRelation
     AnalyticsRelation(
       analytics,
       parameters("ids"),
